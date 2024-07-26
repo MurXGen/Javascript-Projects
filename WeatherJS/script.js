ae37ad79d217f876c34215dd5c7f180667fae4be
Murthy Thevar <152300792+MurXGen@users.noreply.github.com>
2024-07-26 20:04:35 +0530

    Complete

--- WeatherJS/script.js
+++ WeatherJS/script.js
@@ -1,9 +1,52 @@
-function fetchData(callback) {
-    setTimeout(function() {
-      callback("Data received");
-    }, 1000); // Simulates a delay of 1 second
+
+
+const apiKey = "555bb10fd3d3daed71c1ea46770b01aa";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("search_btn");
+const recentSearch = document.getElementById("recentSearches");
+
+let recent = [];
+
+searchButton.addEventListener("click",()=>{
+  const city = searchInput.value;
+  if(city){
+    checkWeather(city);
+    updateCitySearch(city);
   }
-  
-  fetchData(function(message) {
-    console.log(message);
+})
+
+function updateCitySearch(city){
+  if(!recent.includes(city)){
+    recent.push(city);
+    renderCity();
+  }
+}
+
+function renderCity(){
+  recentSearches.innerHTML = "";
+  recent.forEach((city)=>{
+  const citySearch = document.createElement("span");
+  citySearch.textContent = city;
+  citySearch.addEventListener("click",()=>{
+  searchInput.value = city;
+  checkWeather(city);
   });
+  recentSearches.appendChild(citySearch)
+  });
+}
+
+
+async function checkWeather(city){
+  
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+  console.log(data);
+  document.getElementById("cityname").innerHTML = data.name;
+  document.getElementById("speed").innerHTML = data.wind.speed + "Km/Hr"+"<br>"+"Wind Speed";
+  document.getElementById("humidity").innerHTML = data.main.humidity +"<br>"+"Humidity";
+  document.getElementById("feels_cel").innerHTML ="Feels Like " + data.main.feels_like + " C";
+  document.getElementById("cel").innerHTML = Math.round(data.main.temp) + " C";
+}
+
